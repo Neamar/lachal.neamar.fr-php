2459daedd8804173b16a8f9dca3f47f198d3c5e2
Neamar <neamar@neamar.fr>
2017-12-29 17:25:19 +0100

    Added viewport

--- header.php
+++ header.php
@@ -15,6 +15,7 @@ if(!isset($description))
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
 	<title><?php echo $titre; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<meta name="owner" content="Neamar" />
 	<meta name="author" content="Neamar" />
